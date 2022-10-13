c518580cc0160c0023619e1ada0a672f558c5733
HamanShareghi <Haman.Shareghi@gmail.com>
2022-10-13 08:48:47 +0400

    Init Project

---
+++ SiteVisitor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SiteVisitor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
